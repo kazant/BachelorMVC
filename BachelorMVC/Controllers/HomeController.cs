b98dd718440e7646dcddfbbf5afdb184599a259c
JesusFrelseren <ErlendAHall@gmail.com>
2018-02-19 19:51:24 +0100

    sign codebase

--- BachelorMVC/Controllers/HomeController.cs
+++ BachelorMVC/Controllers/HomeController.cs
@@ -11,6 +11,7 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using System.Xml;
+
 using signicat.documentservicev3;
 
 namespace BachelorMVC.Controllers
@@ -62,6 +63,7 @@ namespace BachelorMVC.Controllers
             List<subject> subjects = new List<subject>();
             List<task> tasks = new List<task>();
             List<notification> notices = new List<notification>();
+            List<request> requests = new List<request>();
 
             var methods = new method[]
             {
@@ -163,7 +165,7 @@ namespace BachelorMVC.Controllers
 
                 //Språk(?)
                 // -> Stringparameter (nb for bokmål, nn for nynorsk)
-                
+
                 //Notification
 
                 //Klientreferanse (dokumentpartner)
@@ -175,125 +177,57 @@ namespace BachelorMVC.Controllers
                     //first-name,
                     //last-name
                 //};
-        }
 
-        
+                requests.Add
+                (
+                    new request 
+                        {
+                            //profile,
+                            //language,
+                            subject = subjects.ToArray(),
+                            document = documents.ToArray(),
+                            task = tasks.ToArray(),
+                            clientreference = klientref,
+                            //sender = Senderobjekt,
+                            //notification = Notificationobjekt,
+                            //daysuntildeletion = int,
+                            //account = string 
+                            //packagingtask = //
+                        }
+                );
 
-
-        /* 
-
-        
-        public static void CallWebService()
-{
-    var _url = "https://preprod.signicat.com/ws/documentservice-v3?wsdl";
-    var _action = "http://xxxxxxxx/Service1.asmx?op=HelloWorld";
-
-    XmlDocument soapEnvelopeXml = CreateSoapEnvelope();
-    HttpWebRequest webRequest = CreateWebRequest(_url, _action);
-    InsertSoapEnvelopeIntoWebRequest(soapEnvelopeXml, webRequest);
-
-    // begin async call to web request.
-    IAsyncResult asyncResult = webRequest.BeginGetResponse(null, null);
-
-    // suspend this thread until call is complete. You might want to
-    // do something usefull here like update your UI.
-    asyncResult.AsyncWaitHandle.WaitOne();
-
-    // get the response from the completed web request.
-    string soapResult;
-    using (WebResponse webResponse = webRequest.EndGetResponse(asyncResult))
-    {
-        using (StreamReader rd = new StreamReader(webResponse.GetResponseStream()))
-        {
-            soapResult = rd.ReadToEnd();
-        }
-        Console.Write(soapResult);        
-    }
-}
-
-private static HttpWebRequest CreateWebRequest(string url, string action)
-{
-    HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create(url);
-    webRequest.Headers.Add("SOAPAction", action);
-    webRequest.ContentType = "text/xml;charset=\"utf-8\"";
-    webRequest.Accept = "text/xml";
-    webRequest.Method = "POST";
-    return webRequest;
-}
-
-private static XmlDocument CreateSoapEnvelope()
-{
-    XmlDocument soapEnvelopeDocument = new XmlDocument();
-    soapEnvelopeDocument.LoadXml(@"<SOAP-ENV:Envelope xmlns:SOAP-ENV=""http://schemas.xmlsoap.org/soap/envelope/"" xmlns:xsi=""http://www.w3.org/1999/XMLSchema-instance"" xmlns:xsd=""http://www.w3.org/1999/XMLSchema""><SOAP-ENV:Body><HelloWorld xmlns=""http://tempuri.org/"" SOAP-ENV:encodingStyle=""http://schemas.xmlsoap.org/soap/encoding/""><int1 xsi:type=""xsd:integer"">12</int1><int2 xsi:type=""xsd:integer"">32</int2></HelloWorld></SOAP-ENV:Body></SOAP-ENV:Envelope>");
-    return soapEnvelopeDocument;
-}
-
-private static void InsertSoapEnvelopeIntoWebRequest(XmlDocument soapEnvelopeXml, HttpWebRequest webRequest)
-{
-    using (Stream stream = webRequest.GetRequestStream())
-    {
-        soapEnvelopeXml.Save(stream);
-    }
-}
-
-         public async void UploadToSDS() 
-        {
-            
-            var httpClientHandler = new HttpClientHandler 
-            {
-                Credentials = new NetworkCredential("demo", "Bond007")
-            };
-
-            using (var client = new HttpClient(httpClientHandler)) 
-            {
-                // todo: HTML File som parameter
-                Byte[] file = System.IO.File.ReadAllBytes("Controllers/vedlegg1.pdf");
-                HttpContent content = new ByteArrayContent(file);
-                
-                // Mulighet for flere filtyper og validering
-                content.Headers.ContentType = new MediaTypeHeaderValue("application/pdf");
-                
-                // Dokumentet blir lastet opp til demo-SDS og responsen lagres for videre bruk
-                HttpResponseMessage response = 
-                    await client.PostAsync("https://preprod.signicat.com/doc/demo/sds", content);
-
-                string documentId = await response.Content.ReadAsStringAsync();
-
-                // For testing av respons
-                using(StreamWriter sw = System.IO.File.CreateText("Controllers/output.txt"))
+                var create = new createrequestrequest
                 {
-                    sw.WriteLine(documentId);
+                    password = "Bond007",
+                    service = "demo",
+                    request = requests.ToArray()
+                };
+               
+               var request = new createRequest(create);
+
+               
+                
+     
+                createrequestresponse response;
+                createRequestResponse1 response1;
+
+                using (var client = new signicat.documentservicev3.DocumentEndPointClient())
+                {
+                    response = client.createRequest(create);
                 }
                 
-            }
-
-            // Rut bruker til signicat
-            Dokumenter dokument = new Dokumenter
-            {
-                dokumentID = "dokumentid",
-                dokumentIDFraSDS = "fraSDS",
-                Name = "navn",
-                signert = false,
-                beskrivelse = "beskrivelse"
-
-            };
-
-            var request = new CreateRequest
-            {
-                password = "Bond007",
-                service = "demo",
-                new request {
-                    klientReferanse = "klientreferanse",
-                    language = "nb",
-                    profil = "profil",
-                    dokument = dokument
                 
-                }
-            };
                 
-        
+                
+                
+                 
 
+                
+               
+                
         }
-        */
+
     }
+
+
 }
