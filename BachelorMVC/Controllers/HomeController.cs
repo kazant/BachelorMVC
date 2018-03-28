0ea44ff63534176ea495ee99dc0440b8e7eeb355
Alexander Pettersen <Alex1990pettersen@gmail.com>
2018-03-28 11:51:02 +0200

    endring av prop i bruker - DB i auth0

--- BachelorMVC/Controllers/HomeController.cs
+++ BachelorMVC/Controllers/HomeController.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.Mvc;
 using Assently.Client;
 using Assently.ServiceModel;
 using Assently.ServiceModel.Messages;
+using RestSharp;
+using RestSharp.Authenticators;
 
 namespace BachelorMVC.Controllers
 {
@@ -84,6 +86,24 @@ namespace BachelorMVC.Controllers
 
            
         }
+
+        //////////////////////////////////////////////////////////////////////////////
+
+        public ActionResult getAuth0()
+        {
+            var client = new RestClient("https://document.eu.auth0.com/api/v2/users/auth0|5ab7af1b4d9ffe30cb1334aa");
+            var request = new RestRequest(Method.PATCH);
+            request.AddHeader("content-type", "application/json");
+            request.AddHeader("authorization", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6Ik9UUTJOelJDT1VRNVF6Y3pRakk1TnpReFFUTkZOMEkwTmtZMU56YzBOa1V3TVVFMlJVUXlSQSJ9.eyJpc3MiOiJodHRwczovL2RvY3VtZW50LmV1LmF1dGgwLmNvbS8iLCJzdWIiOiJKbGk5SU0wQXF1QTdYZWlDcW5pcmhPd0FYRmcxSDY4UUBjbGllbnRzIiwiYXVkIjoiaHR0cHM6Ly9kb2N1bWVudC5ldS5hdXRoMC5jb20vYXBpL3YyLyIsImlhdCI6MTUyMjE1MTY1MCwiZXhwIjoxNTIyMjM4MDUwLCJhenAiOiJKbGk5SU0wQXF1QTdYZWlDcW5pcmhPd0FYRmcxSDY4USIsInNjb3BlIjoicmVhZDpjbGllbnRfZ3JhbnRzIGNyZWF0ZTpjbGllbnRfZ3JhbnRzIGRlbGV0ZTpjbGllbnRfZ3JhbnRzIHVwZGF0ZTpjbGllbnRfZ3JhbnRzIHJlYWQ6dXNlcnMgdXBkYXRlOnVzZXJzIGRlbGV0ZTp1c2VycyBjcmVhdGU6dXNlcnMgcmVhZDp1c2Vyc19hcHBfbWV0YWRhdGEgdXBkYXRlOnVzZXJzX2FwcF9tZXRhZGF0YSBkZWxldGU6dXNlcnNfYXBwX21ldGFkYXRhIGNyZWF0ZTp1c2Vyc19hcHBfbWV0YWRhdGEgY3JlYXRlOnVzZXJfdGlja2V0cyByZWFkOmNsaWVudHMgdXBkYXRlOmNsaWVudHMgZGVsZXRlOmNsaWVudHMgY3JlYXRlOmNsaWVudHMgcmVhZDpjbGllbnRfa2V5cyB1cGRhdGU6Y2xpZW50X2tleXMgZGVsZXRlOmNsaWVudF9rZXlzIGNyZWF0ZTpjbGllbnRfa2V5cyByZWFkOmNvbm5lY3Rpb25zIHVwZGF0ZTpjb25uZWN0aW9ucyBkZWxldGU6Y29ubmVjdGlvbnMgY3JlYXRlOmNvbm5lY3Rpb25zIHJlYWQ6cmVzb3VyY2Vfc2VydmVycyB1cGRhdGU6cmVzb3VyY2Vfc2VydmVycyBkZWxldGU6cmVzb3VyY2Vfc2VydmVycyBjcmVhdGU6cmVzb3VyY2Vfc2VydmVycyByZWFkOmRldmljZV9jcmVkZW50aWFscyB1cGRhdGU6ZGV2aWNlX2NyZWRlbnRpYWxzIGRlbGV0ZTpkZXZpY2VfY3JlZGVudGlhbHMgY3JlYXRlOmRldmljZV9jcmVkZW50aWFscyByZWFkOnJ1bGVzIHVwZGF0ZTpydWxlcyBkZWxldGU6cnVsZXMgY3JlYXRlOnJ1bGVzIHJlYWQ6cnVsZXNfY29uZmlncyB1cGRhdGU6cnVsZXNfY29uZmlncyBkZWxldGU6cnVsZXNfY29uZmlncyByZWFkOmVtYWlsX3Byb3ZpZGVyIHVwZGF0ZTplbWFpbF9wcm92aWRlciBkZWxldGU6ZW1haWxfcHJvdmlkZXIgY3JlYXRlOmVtYWlsX3Byb3ZpZGVyIGJsYWNrbGlzdDp0b2tlbnMgcmVhZDpzdGF0cyByZWFkOnRlbmFudF9zZXR0aW5ncyB1cGRhdGU6dGVuYW50X3NldHRpbmdzIHJlYWQ6bG9ncyByZWFkOnNoaWVsZHMgY3JlYXRlOnNoaWVsZHMgZGVsZXRlOnNoaWVsZHMgdXBkYXRlOnRyaWdnZXJzIHJlYWQ6dHJpZ2dlcnMgcmVhZDpncmFudHMgZGVsZXRlOmdyYW50cyByZWFkOmd1YXJkaWFuX2ZhY3RvcnMgdXBkYXRlOmd1YXJkaWFuX2ZhY3RvcnMgcmVhZDpndWFyZGlhbl9lbnJvbGxtZW50cyBkZWxldGU6Z3VhcmRpYW5fZW5yb2xsbWVudHMgY3JlYXRlOmd1YXJkaWFuX2Vucm9sbG1lbnRfdGlja2V0cyByZWFkOnVzZXJfaWRwX3Rva2VucyBjcmVhdGU6cGFzc3dvcmRzX2NoZWNraW5nX2pvYiBkZWxldGU6cGFzc3dvcmRzX2NoZWNraW5nX2pvYiByZWFkOmN1c3RvbV9kb21haW5zIGRlbGV0ZTpjdXN0b21fZG9tYWlucyBjcmVhdGU6Y3VzdG9tX2RvbWFpbnMgcmVhZDplbWFpbF90ZW1wbGF0ZXMgY3JlYXRlOmVtYWlsX3RlbXBsYXRlcyB1cGRhdGU6ZW1haWxfdGVtcGxhdGVzIiwiZ3R5IjoiY2xpZW50LWNyZWRlbnRpYWxzIn0.GKVkM_zFJnVG_9nPm7AN34EAxamKIJw9Yoo_0X_p8JTVUi4oIwOAb5q0T9qFskKVDnTQkDih4eenL6Wb-0qXJQMQbc6-XFJzW4E4DbKN1DMcV8lzKNgUBvjj3IzGBsRfYKJKapEsIijVkaWDaDtl0P2V5YQhUeREmDJZuY3beyL9ZNWNaLdNV1-ik3AxATZAX44HfzOM-g2uI_x7x0ssqDMUAuaPiSgsGlC1ir32wwfRI0sXH0vq_9THAW1mAqjikmrQYVI5RyQd0Bf-JcgBG4clsx16N_njedqIcJqCQe9IVDjK1yMz67cKM9lkcF7C4N0dVnHEwziL0DZCt3rsbg");
+            request.AddParameter("application/json", "{\"user_metadata\": {\"godkjent\": \"0\"}}", ParameterType.RequestBody);
+            //request.AddParameter("application/json", "{\"user_metadata\": {\"addresses\": {\"home\": \"123456 Main Streetssss, Anytown, ST 12345\"}}}", ParameterType.RequestBody);
+            IRestResponse response = client.Execute(request);
+
+            return View("Innlogget");
+        }
+
+
+        //////////////////////////////////////////////////////////////////////////////
 
 
         public IActionResult LagDokumentGrid()
